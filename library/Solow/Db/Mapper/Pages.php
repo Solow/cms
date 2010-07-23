af439d7757ad81b9069e4cc8d654c6c6a6ae4024
Wesley Overdijk <mathematica@live.nl>
2010-07-24 01:42:35 +0200

    Made some personal notes

--- library/Solow/Db/Mapper/Pages.php
+++ library/Solow/Db/Mapper/Pages.php
@@ -18,6 +18,26 @@ class Solow_Db_Mapper_Pages extends Solow_Db_Mapper_Abstract
             )
         );
         $resTables = $this->fetchAllSingle($fetchTable, 'Customers_Model_Customers');
+
+        /*
+         * Wat sla ik op bij een pagina?
+         * - Controller
+         * - Action
+         * - slug
+         *
+         * Waar match ik op?
+         * - slug OF page/sub/sub/sub, where the current used 'lets say _' is replaced by '/', so i can match.
+         * - id (ligt aan de settings)
+         *
+         * mogelijke methodes:
+         * - slug
+         * - parent/child/child/
+         * - /id
+         *
+         * gebruik:
+         * voor Zend_Navigation_Page, de uri.
+         * http://framework.zend.com/manual/en/zend.navigation.pages.html#zend.navigation.pages.uri
+         */
     }
 }
 ?>
