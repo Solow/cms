ff50ed8ce91499f2997bc685e912447413b8ce03
Wesley Overdijk <mathematica@live.nl>
2010-07-27 16:53:46 +0200

    added controller and action

---
+++ application/controllers/PagesController.php
@@ -0,0 +1,25 @@
+<?php
+
+class PagesController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function pageAction()
+    {
+        // action body
+    }
+
+
+}
+
+
+
